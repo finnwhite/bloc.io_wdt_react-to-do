26479bbc4458c12ab4c8703e2939e803e310a90a
Finn White <37319636+finnwhite@users.noreply.github.com>
2018-06-26 15:43:57 -0700

    Create ToDo item component
    Created static/dummy ToDo item component, added to App.
    
    Completes Intro to React > CP06 React To-Do: Components

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li>A todo item goes here</li>
+    );
+  }
+}
+
+export default ToDo;
