a3a81358ac37e6cfd80e48ab41ace194be986fd7
Finn White <37319636+finnwhite@users.noreply.github.com>
2018-06-27 11:47:26 -0700

    Update App, ToDo components
    Added state to App component, props to ToDo component to enable dynamic
    content display.
    
    Completes Intro to React > CP07 React To-Do: State and Props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: "Take the baby to daycare", isCompleted: true },
+        { description: "Pick the baby up from daycare", isCompleted: false },
+        { description: "Change the baby's diaper", isCompleted: false },
+        { description: "Feed the baby dinner", isCompleted: false },
+        { description: "Give the baby a bath", isCompleted: false },
+        { description: "Put the baby in pajamas", isCompleted: false },
+        { description: "Read the baby a bedtime story", isCompleted: false },
+        { description: "Feed the baby a bottle", isCompleted: false },
+        { description: "Put the baby to bed", isCompleted: false }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo
+              key={ index }
+              description={ todo.description }
+              isCompleted={ todo.isCompleted }
+            />
+          )}
         </ul>
       </div>
     );
